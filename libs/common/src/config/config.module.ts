c7f2dc90e99296e7a8dad3269c0b43ff650d984e
Loïc Thebault <loic.thebault18@gmail.com>
2024-01-13 22:54:52 +0100

    finalize common module setup

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [NestConfigModule.forRoot({
+    validationSchema: Joi.object({ MONGODB_URI: Joi.string().required() })
+  })],
   providers: [ConfigService],
   exports: [ConfigService]
 })
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI')
+      }),
+      inject: [ConfigService]
+    })
+  ]
+})
+
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models)
+  }
+}
